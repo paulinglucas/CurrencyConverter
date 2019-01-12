79af76570eab7f40d4938a573c944a15ff11bc78
paulinglucas <paulinglucas@gmail.com>
2019-01-12 15:30:47 -0600

    added options to spinner objects

--- app/src/main/java/com/example/snork/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/snork/currencyconverter/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.snork.currencyconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.currencies_array,
+                android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+        spinner2.setAdapter(adapter);
+
     }
 }
